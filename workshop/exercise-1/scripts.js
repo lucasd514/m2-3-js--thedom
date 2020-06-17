90744f7748e96e27c74f811586933b6df2e8fb9d
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-17 13:43:02 -0400

    q1push

--- workshop/exercise-1/scripts.js
+++ workshop/exercise-1/scripts.js
@@ -1,1 +1,8 @@
 // Add your code here!
+const container = document.querySelector(main)
+
+//Main title
+
+const title = document.createElement('h1')
+title.innerText = 'text';
+container.appendChild(title);
